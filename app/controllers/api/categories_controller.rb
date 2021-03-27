12d96a391b79737b690babe9c04c21a5391f6f20
Carrie Mathieu <carrieburks@gmail.com>
2021-03-27 16:07:03 -0400

    categories index/create

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,21 @@
 class Api::CategoriesController < ApplicationController
+    def index
+        categories = Category.all 
+        render json: categories
+    end
+
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: category, status: :accepted
+        else
+            render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end

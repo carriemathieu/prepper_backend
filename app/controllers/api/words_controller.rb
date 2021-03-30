ea1c6131ed6b16a6dfa6fff087d776d16d5911a2
Carrie Mathieu <carrieburks@gmail.com>
2021-03-30 16:18:58 -0400

    words controller create method

--- app/controllers/api/words_controller.rb
+++ app/controllers/api/words_controller.rb
@@ -3,4 +3,20 @@ class Api::WordsController < ApplicationController
         words = Word.all 
         render json: WordSerializer.new(words)
     end
+
+    def create
+        words_list = Word.new(words_params)
+        byebug
+        if words_list.save
+            render json: WordSerializer.new(words_list), status: :accepted
+        else
+            render json: {errors: word_list.errors.full_messages}
+        end
+    end
+
+    private
+
+    def words_params
+        params.require(:word).permit("title", "category_id", :word_list => [])
+    end
 end

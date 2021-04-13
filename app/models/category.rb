29d1b7afbf3c7a35099ed3bf2ea171aa5e7cbd79
Carrie Mathieu <carrieburks@gmail.com>
2021-04-13 01:05:41 -0400

    added uniqueness validation to categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     has_many :words, dependent: :destroy
-    validates :name, presence: true
-    # validates :name, uniqueness: true   
+    validates :name, presence: true, uniqueness: true  
+    # validates :name,  
 end
